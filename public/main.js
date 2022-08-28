a08b0158a391711ce1c46896c2b03072991274b8
Tobi Balogun <tobi.balo@gmail.com>
2022-08-28 12:06:24 +0100

    Complete server side code

--- public/main.js
+++ public/main.js
@@ -1,1 +1,29 @@
-alert("working")
+const form = document.querySelector("#form")
+const h2ForName = document.querySelector("#namePlace")
+const rapName = document.querySelector("#rapName")
+const rapperName = document.querySelector("#rapperName")
+
+rapperName.addEventListener("focus", () => {
+  h2ForName.classList.add("hidden")
+})
+form.addEventListener("submit", getRapName)
+
+async function getRapName(e) {
+  e.preventDefault()
+  const inputBox = document.querySelector("#rapperName")
+  const rapperName = inputBox.value.toLowerCase()
+  inputBox.value = ""
+  if (!rapperName) {
+    console.log("Give me something, dawg!")
+    return
+  }
+  const URL = `https://rapsupreme-api.herokuapp.com/api/${rapperName}`
+  const fetchRapObject = await fetch(URL)
+  const rapObject = await fetchRapObject.json()
+  displayName(rapObject.birthName)
+}
+
+function displayName(name) {
+  rapName.innerHTML = name
+  h2ForName.classList.remove("hidden")
+}
